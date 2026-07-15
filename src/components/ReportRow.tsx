f25478cf6d650555c62c2bacb6108fa657cfda03
spikereloaded <lewis.198804@gmail.com>
2026-07-15 22:46:32 +0100

    Styling - Added styling to more info div for each report row. Added warning icon for members who didn't reach expected hit count

--- src/components/ReportRow.tsx
+++ src/components/ReportRow.tsx
@@ -6,7 +6,7 @@ interface membersArmouryNews {
     xanaxUsed: number
     medsUsed: number
     ipecacUsed: number
-    attackPotential: number
+    attackPotential: number 
 }
 
 function ReportRow({ memberId, memberName, memberAttacks, participationNumber, participationBarWidth, participationBarColour, memberScore, armouryTime, warEndDate, armouryNews }: MemberRow) {
@@ -36,7 +36,7 @@ function ReportRow({ memberId, memberName, memberAttacks, participationNumber, p
 
         const xanaxFiltered = preFiltered.filter(newsItem => newsItem.text.includes("Xanax"))
         const xanaxGain = xanaxFiltered.length * xanaxEnergyGain
-        const attackPotential = ((150 + xanaxGain) / 25)
+        const attackPotential = xanaxGain / 25
         const medsFiltered = preFiltered.filter(newsItem =>
             newsItem.text.includes("Morphine") || newsItem.text.includes("First Aid Kit")
         )
@@ -51,12 +51,15 @@ function ReportRow({ memberId, memberName, memberAttacks, participationNumber, p
 
     }, [armouryNews])
 
+   const warningIcon =  filteredNews && filteredNews.attackPotential > memberAttacks ? <i className="warning-icon bi bi-exclamation-triangle-fill"></i>
+        : null
     return (
         <div className="row-container">
             <div className="member-row" onClick={showMore}>
                 <div className="participation-bar" style={{ width: `${participationBarWidth}`, background: `${participationBarColour}` }}></div>
-                <p>{memberName} </p><p className="respect-p">Respect earned: {memberScore}</p><p><i className="bi bi-bullseye"></i> {memberAttacks} <span className="participation"> ({participationNumber}%)</span></p>
+                <p>{memberName} {warningIcon} </p><p className="respect-p">Respect earned: {memberScore}</p><p><i className="bi bi-bullseye"></i> {memberAttacks} <span className="participation"> ({participationNumber}%)</span></p>
                 {filteredNews ?
+
                     <p>{filteredNews.xanaxUsed} | {filteredNews.attackPotential} - {filteredNews.medsUsed} - {filteredNews.ipecacUsed}</p>
                     : null
                 }
@@ -67,6 +70,20 @@ function ReportRow({ memberId, memberName, memberAttacks, participationNumber, p
                     <h3>More Info</h3>
                     <p><a href={`https://www.torn.com/messages.php#/p=compose&XID=${memberId}`} target="_blank"><i className="bi bi-envelope-arrow-up"></i></a> <a href={`https://www.torn.com/profiles.php?XID=${memberId}`} target="_blank"><i className="bi bi-person-circle"></i></a></p>
                     <p className="average-p">{memberName} averaged {averageRespect} respect per attack.</p>
+                    <div className="stats-container">
+                        <div>
+                            <h4>Xanax used:</h4>
+                            <p>{filteredNews?.xanaxUsed}</p>
+                        </div>
+                        <div>
+                            <h4>Expected Attacks:</h4>
+                            <p>{filteredNews?.attackPotential}</p>
+                        </div>
+                        <div>
+                            <h4>Meds Used:</h4>
+                            <p>{filteredNews?.medsUsed}</p>
+                        </div>
+                    </div>
                 </div>
                 : null
             }
