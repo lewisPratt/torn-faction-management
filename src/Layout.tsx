40873ecba80190193a1cfc73c605df708b2a91d3
spikereloaded <lewis.198804@gmail.com>
2026-07-22 12:32:16 +0100

    Styling / Feature - added buymeacoffee button to footers. Added hover effect to statscontainer children divs.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -11,6 +11,9 @@ function Layout({ children, handleClearKey }: LayoutProps) {
             <footer>
                 <ClearKeyButton handleClearKey={handleClearKey} />
                 <a href="https://github.com/lewisPratt/torn-faction-management/issues" target="_blank"><button>Bugs & Feature Requests</button></a>
+                <a href="https://buymeacoffee.com/lewis19880g" target="_blank"><button>Buy me a Coffee!</button></a>
+
+                
             </footer>
         </>
     )
--- src/components/ApiKeyForm.tsx
+++ src/components/ApiKeyForm.tsx
@@ -23,7 +23,7 @@ function ApiKeyForm({ inputValue, setInputValue, handleSubmit }: ApiKeyFormProps
             <br /><button onClick={handleSubmit}>Login</button></p>
         </div>
       </main>
-      <footer><a href="https://github.com/lewisPratt/torn-faction-management/issues" target="_blank"><button>Bugs & Feature Requests</button></a></footer>
+      <footer><a href="https://github.com/lewisPratt/torn-faction-management/issues" target="_blank"><button>Bugs & Feature Requests</button></a><a href="https://buymeacoffee.com/lewis19880g" target="_blank"><button>Buy me a Coffee!</button></a></footer>
     </>
   )
 }
