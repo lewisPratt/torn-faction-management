0edd009c461692882c3df18337348fcbe35e9a39
spikereloaded <lewis.198804@gmail.com>
2026-07-22 23:26:04 +0100

    Feature - Added latest news component to login page. added css to animate

--- src/components/ApiKeyForm.tsx
+++ src/components/ApiKeyForm.tsx
@@ -1,5 +1,6 @@
 import type { ApiKeyFormProps } from "../interfaces"
 import DarkMode from "./DarkMode"
+import LatestUpdates from "./LastestUpdates"
 
 function ApiKeyForm({ inputValue, setInputValue, handleSubmit }: ApiKeyFormProps) {
  
@@ -24,6 +25,7 @@ function ApiKeyForm({ inputValue, setInputValue, handleSubmit }: ApiKeyFormProps
           />
             <br /><button onClick={handleSubmit}>Login</button></p>
         </div>
+        <div id="latest-news"><LatestUpdates /> </div>
       </main>
       <footer><a href="https://github.com/lewisPratt/torn-faction-management/issues" target="_blank"><button>Bugs & Feature Requests</button></a><a href="https://buymeacoffee.com/lewis19880g" target="_blank"><button>Buy me a Coffee!</button></a> <DarkMode />
 </footer>
---
+++ src/components/LastestUpdates.tsx
@@ -0,0 +1,13 @@
+
+
+function LatestUpdates(){
+
+return(
+    <ul>
+        <li><i className="bi bi-cup-hot-fill"></i> Implemented Dark Mode toggle! Seismic feature! <i className="bi bi-cup-hot-fill"></i></li>
+    </ul>
+)
+
+}
+
+export default LatestUpdates
