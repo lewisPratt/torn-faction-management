0a2021b4f29e4c55d330386b49d3a769f768527c
spikereloaded <lewis.198804@gmail.com>
2026-08-01 22:45:25 +0100

    Feature - Added function to memebrpayout component to allow toggling of styling to indicate paid and un paid war shares

--- src/components/MemberPayout.tsx
+++ src/components/MemberPayout.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react"
 
-
-interface memberPayoutProps{
+interface memberPayoutProps {
     payout: string
 }
-function MemberPayout({payout} : memberPayoutProps){
+function MemberPayout({ payout }: memberPayoutProps) {
+    const backgroundString: string = "lightgreen"
+    const textDecString: string = "line-through"
+    const [backgroundCol, setBackgroundCol] = useState<string>()
+    const [textDec, setTextDec] = useState<string>()
 
-    if(payout != "0"){
-        return <div className="payout-container">Share: ${payout}</div>
-    }else{
-        return
+
+    function CheckOff() {
+        console.log(backgroundCol)
+        if (!backgroundCol) {
+            setBackgroundCol(backgroundString)
+            setTextDec(textDecString)
+        } else if (backgroundCol === backgroundString) {
+            setBackgroundCol("")
+            setTextDec("")
+        }
+    }
+
+    if (payout != "0") {
+        return <div className="payout-container" style={{ background: backgroundCol, textDecoration: textDec}} onClick={CheckOff}>Share: ${payout}</div>
+    } else {
+        return null
     }
 
 }
